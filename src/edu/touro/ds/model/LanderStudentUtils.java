24310f1936ac06461aec90f936f74551726a78c2
Michael J Cohen <53616290+lcmcohen@users.noreply.github.com>
2023-12-25 15:23:12 -0500

    Add bubblesort with debug info, add best and worst case data

--- src/edu/touro/ds/model/LanderStudentUtils.java
+++ src/edu/touro/ds/model/LanderStudentUtils.java
@@ -18,13 +18,78 @@ public class LanderStudentUtils {
      * @return List of Students
      */
     static public List<LanderStudent> generateDemoStudents() {
+
+        /* worst case  */
         return Arrays.asList(
-                new LanderStudent("Sill", "Daniel", true, 2000),
-                new LanderStudent("Greenspan", "Steve", true, 2002),
+                new LanderStudent("Sill", "Daniel", true, 2002),
+                new LanderStudent("Greenspan", "Steve", true, 2000),
                 new LanderStudent("Kennedy", "Tom", true, 1999),
                 new LanderStudent("Artzt", "Russell", true, 1955)
         );
+        /****/
+
+        /* medium case
+        return Arrays.asList(
+
+                new LanderStudent("Greenspan", "Steve", true, 2000),
+                new LanderStudent("Artzt", "Russell", true, 1955),
+                new LanderStudent("Kennedy", "Tom", true, 1999),
+                new LanderStudent("Sill", "Daniel", true, 2002)
+
+        );
+        /****/
+
+
+        /* best case
+        return Arrays.asList(
+                new LanderStudent("Sill", "Daniel", true, 2002),
+                new LanderStudent("Greenspan", "Steve", true, 2000),
+                new LanderStudent("Kennedy", "Tom", true, 1999),
+                new LanderStudent("Artzt", "Russell", true, 1955)
+        );
+        /*****/
+
     }
 
 
+    /**
+     * Demo bubblesort
+     * <a href="https://stackoverflow.com/questions/5370829/bubblesort-over-other-sorting-algorithms">...</a>
+     * @param persons a collection to be sorted using naural order of its objects
+     */
+    public static void bubbleSort(List<LanderStudent> persons) {
+int  _ncompare=0, _nswap =0;
+dbugprint("\nbefore bubblesort: ncomp:_nswap  " + _ncompare +":" + _nswap, persons);
+
+        for (int i = 0; i < persons.size() - 1; i++) {
+            boolean allSorted = true;
+            for (int j = 0; j < (persons.size() - 1) - i; j++ ){
+_ncompare++;
+dbugprint("   i/j ncomp:_nswap " + i + "/" + j + " " + _ncompare +":" + _nswap, persons);
+
+                if (persons.get(j).compareTo(persons.get(j+1)) > 0)  {
+_nswap++;
+System.out.println("      SWAP: move right>>> " + persons.get(j).getYearOfBirth());
+                    LanderStudent temp = persons.get(j);
+                    persons.set(j, persons.get(j+1));
+                    persons.set(j+1, temp);
+                    allSorted = false;
+                }
+
+            }
+            if (allSorted)
+                break;
+        }
+
+dbugprint("after bubble" +
+        "sort: ncomp:_nswap " + _ncompare +":" + _nswap, persons);
+    }
+
+    private static void dbugprint(String msg, List<LanderStudent> persons) {
+        System.out.print(msg + " -> ");
+        for (LanderStudent ls : persons) {
+            System.out.print(ls.getYearOfBirth() + ":" + ls.getLastName() + ", ");
+        }
+        System.out.println();
+    }
 }
